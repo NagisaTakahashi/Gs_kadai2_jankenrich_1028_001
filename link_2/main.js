05d81b7761c16662b33462bc56febd036b49ea80
nagisa_takahashi@recouture-inc.com <nagisa_takahashi@outlook.jp>
2022-10-31 17:49:00 +0900

    JSリンク先変更

--- link_2/main.js
+++ link_2/main.js
@@ -1,6 +1,12 @@
-$(function(){
+
+
+
+$.getScript('../main.js');
+$.getScript('main.js');
+
+// $(function(){
   $("#recommend_item").on('click', function(){
     console.log(C[A]);
     $(".kokkaku_type_eng").text(C[A]); //英語表記したいので配列Cを使用
   });
-  });
+// });
